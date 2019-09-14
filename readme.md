4e5820e334487bf97fded3c303523a6d9c388859
Victor Rahael <vicraph7@gmail.com>
2019-09-14 10:58:56 +0100

    having JSON issues

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Hello Developer
+
+I'm currently having issues with my json data. There seems to be something wrong with it
+
+Check line 31 in the script.js file. Since there are sub objects within the big object, I would like to work with those sub objects, but it doesn't seem to be working.
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const $ = b => document.querySelector(b);
 const log = n => console.log(n);
 
+const projects = $('.projects');
+
 const enable_modal = () => {
   const modal = $('.modal');
   modal.style.display = "block";
@@ -10,6 +12,52 @@ const enable_modal = () => {
 }
 $('#add-project').onclick = enable_modal;
 $('.close_modal').onclick = () => $('.modal').style.display = "none";
+
+$('#form1').addEventListener('submit', e => {
+  e.preventDefault();
+})
+$('#addProject').onclick = () => {
+  $('.modal').style.display = "none";
+  log($('.modal'))
+}
+const qs = (n, elem) => n.querySelector(elem);
+
+async function addFromJSON() {
+  const response = await fetch("./projects.json");
+  const data = await response.json();
+  
+  let frag = document.createDocumentFragment();
+  for (let i in data) {
+    for (let j in i) log(j);
+    // const { title, img_src, tags, description } = i;
+    // let clonedTemplate = $('.project').cloneNode(true);
+    // qs(clonedTemplate, '.title').textContent = title;
+    // qs(clonedTemplate, '.info').textContent = description;
+    // qs(clonedTemplate, '.info').textContent = description;
+    // frag.appendChild(clonedTemplate);
+  }
+  $('.projects').appendChild(frag);
+  delete frag;
+}
+addFromJSON();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 const projects = $('.projects');
@@ -22,7 +70,7 @@ const add_project = () => {
 
   //alert('eff')
   //const b = "border: 1px solid #509fbe;margin: 2bw auto;border-radius: 5px;padding: 20px;line-height: 2em;display: flex;flex-flow: column wrap;justify-content: center;"
-  
+
   let a = `
     <div>
       <p>Project Name: ${project_name.value}</p>
